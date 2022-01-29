2f6ce010ae5501f42814bb7beb5d778a4b454021
artem <poi.ru0130@gmail.com>
2022-01-29 23:11:54 +0300

    add  attach video

--- src/test/java/ru/zets/tests/TestBase.java
+++ src/test/java/ru/zets/tests/TestBase.java
@@ -25,7 +25,7 @@ public class TestBase {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC",true);
         capabilities.setCapability("enableVideo",true);
-        Configuration.browserCapabilities =capabilities;
+        Configuration.browserCapabilities = capabilities;
 
     }
     @AfterEach
@@ -33,6 +33,7 @@ public class TestBase {
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
         Attach.browserConsoleLogs();
+        Attach.addVideo();
 
     }
 
