3c315ca2d7ecbf0e100fc4197e165c2e7ee3436b
artem <poi.ru0130@gmail.com>
2022-01-29 23:08:38 +0300

    add selenoid and attach

--- src/test/java/ru/zets/tests/TestBase.java
+++ src/test/java/ru/zets/tests/TestBase.java
@@ -1,7 +1,14 @@
 package ru.zets.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.Description;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import ru.zets.pages.RegistrationPage;
 import ru.zets.data.DataGen;
 
@@ -11,5 +18,22 @@ public class TestBase {
     public RegistrationPage registrationPage = new RegistrationPage();
 
     @BeforeAll
-    static void beforeAll() { Configuration.startMaximized = true; }
+    static void setup(){
+        Configuration.startMaximized = true;
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC",true);
+        capabilities.setCapability("enableVideo",true);
+        Configuration.browserCapabilities =capabilities;
+
+    }
+    @AfterEach
+    public void teaedDown(){
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+
+    }
+
 }
