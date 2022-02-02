ee8be7a74e7fea1a5dee56fa45803da74fe6dac1
artem <poi.ru0130@gmail.com>
2022-02-02 17:38:06 +0300

    add @Tag

--- src/test/java/ru/zets/tests/TestBase.java
+++ src/test/java/ru/zets/tests/TestBase.java
@@ -2,26 +2,34 @@ package ru.zets.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.CredentialsConfig;
 import helpers.Attach;
 import io.qameta.allure.Description;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import ru.zets.pages.RegistrationPage;
 import ru.zets.data.DataGen;
 
+@Tag("Owner")
 public class TestBase {
 
     public DataGen data = new DataGen();
     public RegistrationPage registrationPage = new RegistrationPage();
-
+    public CredentialsConfig credentials =
+            ConfigFactory.create(CredentialsConfig.class);
+    String login = credentials.login();
+    String password = credentials.password();
     @BeforeAll
     static void setup(){
         Configuration.startMaximized = true;
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        String selenoid = System.getProperty("selenoid","selenoid.autotests.cloud/wd/hub");
+        Configuration.remote = "https://%s:%s@"+selenoid;
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC",true);
         capabilities.setCapability("enableVideo",true);
--- src/test/java/ru/zets/tests/Tests.java
+++ src/test/java/ru/zets/tests/Tests.java
@@ -1,8 +1,12 @@
 package ru.zets.tests;
 
+import config.CredentialsConfig;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 
+@Tag("Owner")
 public class Tests extends TestBase {
     @Test
     void RegistrationPageTest(){
